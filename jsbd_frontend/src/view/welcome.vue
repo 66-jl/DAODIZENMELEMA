5d93daa1de40a7261648fdd80abfc6575506617c
66-jl <2977752994@qq.com>
2026-08-23 22:23:15 +0800

    引入并测试ElementPlus

--- jsbd_frontend/src/view/welcome.vue
+++ jsbd_frontend/src/view/welcome.vue
@@ -3,6 +3,17 @@
         hello
 
     </div>
+    <el-row>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <el-icon class="el-icon-circle-plus-outline">1</el-icon>
+    <div>  </div>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
 
 
 </template>
